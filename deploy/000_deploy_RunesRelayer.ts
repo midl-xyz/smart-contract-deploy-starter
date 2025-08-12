0ef960f99dcd7ff592f776db4753fa4a1c8b309e
georgyskr <gosha.skryuchenkov@gmail.com>
2025-08-12 11:20:58 +0300

    feat: update to the latest hh option

--- deploy/000_deploy_RunesRelayer.ts
+++ deploy/000_deploy_RunesRelayer.ts
@@ -1,33 +1,13 @@
-import type { DeployFunction } from "hardhat-deploy/types";
 import { erc20 } from "@/typechain-types/factories/@openzeppelin/contracts/token";
-import { MaxUint256, parseEther } from "ethers";
+import type { DeployFunction } from "hardhat-deploy/types";
 
 const deploy: DeployFunction = async ({ midl }) => {
 	console.log("Starting distribute Tokens...");
-
-	// Initialize Midl
 	await midl.initialize();
 	const EVMAddress = midl.getEVMAddress();
 	console.log("Performing Admin Based minting with wallet: ", EVMAddress);
 
-	// const poolConfig = PoolConfig;
-	// const reservesConfig = poolConfig.ReservesConfig;
-	// const reserveSymbols = Object.keys(reservesConfig);
-
-	// if (reserveSymbols.length === 0 || !IsDeployNewContract) {
-	// 	console.warn(
-	// 		"[TokenDistribution] PASS! # Token Symbol in Config is zero or Token is not deployed on Testnet",
-	// 		IsDeployNewContract,
-	// 		reserveSymbols,
-	// 	);
-	// 	return;
-	// }
-
 	const RuneID = {
-		// USDC: {
-		// 	runeId: "37535:8",
-		// 	address: "0xb73D9EaB03bCEEE575544353407cc03a606c31bB",
-		// },
 		USDT: {
 			runeId: "11893:1",
 			address: "0x3eDb3dFD4C8b1bb46304F25e933816A7fDAB6FF6",
@@ -38,19 +18,12 @@ const deploy: DeployFunction = async ({ midl }) => {
 		abi: erc20.ERC20__factory.abi as any,
 		address: RuneID.USDT.address as any,
 	});
-	// await midl.save("USDC", {
-	// 	abi: erc20.ERC20__factory.abi as any,
-	// 	address: RuneID.USDC.address as any,
-	// });
 
-	// for (const symbol of ["USDT", "USDC"] as const) {
 	const runeId = RuneID["USDT"].runeId;
 	const runeAddress = RuneID["USDT"].address;
 	const amount = 228n;
-	console.log("USDT", runeId, runeAddress);
 
 	await midl.deploy(`USDTRunesRelayer`, { args: [runeAddress] });
-	await midl.execute(); // Run callContract
 
 	const Relayer = await midl.getDeployment(`USDTRunesRelayer`);
 	console.log("Deployed Relayer Address: ", Relayer?.address);
@@ -74,10 +47,8 @@ const deploy: DeployFunction = async ({ midl }) => {
 		args: [amount],
 	});
 	await midl.execute({
-		options: {
-			withdraw: {
-				runes: [{ id: runeId, amount: amount, address: runeAddress as any }],
-			},
+		withdraw: {
+			runes: [{ id: runeId, amount: amount, address: runeAddress as any }],
 		},
 	});
 
