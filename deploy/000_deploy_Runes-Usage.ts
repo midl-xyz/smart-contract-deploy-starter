7d5fd581414659b2ffe0ac077f14f769322ff0b2
annazhlb <annacher714@gmail.com>
2026-01-21 12:40:39 +0000

    feat: update deploy scripts

---
+++ deploy/000_deploy_Runes-Usage.ts
@@ -0,0 +1,61 @@
+import type { DeployFunction } from "hardhat-deploy/types";
+import { parseEther } from "ethers";
+import { erc20 } from "@/typechain-types/factories/@openzeppelin/contracts/token";
+
+const deploy: DeployFunction = async ({ midl }) => {
+  console.log("Starting deployment process...");
+
+  await midl.initialize();
+  console.log("Deployer BTC address: ", midl.getAccount().address);
+
+  // Prerequisites: have a Rune that's added to the midl
+  const runeAddress = "0xb73D9EaB03bCEEE575544353407cc03a606c31bB";
+
+  await midl.save("TERC20", {
+    address: runeAddress,
+    abi: [...erc20.ERC20__factory.abi],
+  });
+
+  await midl.deploy("RunesRelayer", { args: [runeAddress] });
+
+  await midl.execute();
+
+  // Prerequisites: have a Rune that's added to the midl
+  const runeId = "37535:8";
+  const amount = parseEther("1");
+
+  await midl.deploy("RunesRelayer", { args: [runeAddress] });
+
+  const Relayer = await midl.getDeployment("RunesRelayer");
+
+  await midl.callContract("TERC20", "approve", {
+    args: [Relayer?.address, amount],
+    //to: runeAddress,
+  });
+
+  await midl.callContract(
+    "RunesRelayer",
+    "depositRune",
+    { args: [amount] },
+    {
+      hasRunesDeposit: true,
+      runes: [{ id: runeId, value: amount, address: runeAddress }],
+    },
+  );
+
+  await midl.callContract(
+    "RunesRelayer",
+    "withdrawRune",
+    { args: [amount] },
+    {
+      hasRunesWithdraw: true,
+      runes: [{ id: runeId, value: amount, address: runeAddress }],
+    },
+  );
+
+  await midl.execute({ assetsToWithdraw: [runeAddress], shouldComplete: true });
+};
+
+deploy.tags = ["main", "Runes"];
+
+export default deploy;
